8c6f027555b1aae2befdf2b7268381d424f71421
Shantanupawar-git <shantanu.sp532@gmail.com>
2025-04-14 13:57:01 +0530

    basj

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,5 +1,5 @@
-create database hackathon
-use hackathon
+create database hackathon;
+use hackathon;
 
 create table user
 (id int primary key auto_increment,
@@ -8,7 +8,7 @@ email varchar(30),
 password varchar(100),
 phone_no varchar(10),
 created_time DATETIME default CURRENT_TIMESTAMP
-)
+);
 
 create table blogs
 (id int primary key auto_increment,
@@ -16,10 +16,12 @@ title varchar(20),
 contents varchar(30),
 created_time datetime default current_timestamp ,
 user_Id int,
-category_Id int
-)
+category_Id int,
+FOREIGN KEY (user_Id) REFERENCES user(id),
+FOREIGN KEY (category_Id) REFERENCES categories(id)
+);
 
 create table categories
 (id int primary key auto_increment,
 title varchar(30),
-description varchar(50))
+description varchar(50));
